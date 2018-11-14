ce831cdf37c445324e02fb9745b02fb6ded7d54b
James Cherven <jcherven@mail.usf.edu>
2018-11-14 13:11:15 -0500

    Basic display pages, node server, and routes added

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,20 @@
+// API endpoint routing
+/* 
+Your apiRoutes.js file should contain two routes:
+    A GET route with the url /api/friends. This will be used to display a JSON of all possible friends.
+    A POST routes /api/friends. This will be used to handle incoming survey results. This route will also be used to handle the compatibility logic.
+*/
+
+const path = require('path');
+
+module.exports = function(app) {
+    app.get('/api/friends', function(req, res) {
+        console.log('API endpoint hit" http get');
+
+    });
+
+    app.post('/api/friends', function(req, res){
+        console.log('API enpoint hit: http post');
+
+    });
+};
